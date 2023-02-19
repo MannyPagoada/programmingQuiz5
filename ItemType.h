3e59d9078c3635b94502b90864f3fe72c4660b6d
manuelpagoada <mpagoada@gmail.com>
2023-02-18 16:05:47 -0800

    set up the datatype and sorted

--- ItemType.h
+++ ItemType.h
@@ -5,4 +5,20 @@
 #ifndef PROGRAMMINGQUIZ5_ITEMTYPE_H
 #define PROGRAMMINGQUIZ5_ITEMTYPE_H
 
+
+#include <fstream>
+const int MAX_ITEMS = 5;
+enum RelationType  {LESS, GREATER, EQUAL};
+
+class ItemType
+{
+public:
+  ItemType();
+  RelationType ComparedTo(ItemType) const;
+  void Print(std::ostream&) const;
+  void Initialize(int number);
+private:
+  int value;
+};
+
 #endif //PROGRAMMINGQUIZ5_ITEMTYPE_H
